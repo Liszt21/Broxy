97628e1207a23f90067d574171539b24baec8153
Liszt21 <1832666492@qq.com>
2020-11-10 13:20:24 +0800

    add fetcher to Broxy

--- src/broxy/__init__.py
+++ src/broxy/__init__.py
@@ -1,26 +1,27 @@
 __VERSION__ = "0.0.1"
 
-def main():
-    print("Broxy!")
-
 class Broxy():
     def __init__(self):
         self._pool = []
         self._sources = {}
         self._statistic = {}
+        self._fetcher = None
 
     def source(self, override=False):
         def decorator(f):
             if override or not f.__name__ in self._sources.keys():
                 self._sources[f.__name__] = f
+                self._fetcher = self.fetch()
             else:
                 print("Source {} is already exist!".format(f.__name__))
             return f
         return decorator
 
     def fetch(self):
-        pass
-
+        for source in self._sources.values():
+            for proxy in source():
+                yield proxy
+            
     def get(self,num=1):
         pass
 
@@ -30,16 +31,22 @@ class Broxy():
     def verify(self, p):
         return False
 
-if __name__ == "__main__":
+def main():
+    print("Broxy!")
     broxy = Broxy()
 
     @broxy.source()
     def test():
-        return 1
+        for i in range(1234, 65536):
+            yield {
+                "ip": "1.2.3.4",
+                "port": i,
+                "protocol": "http"
+            }
 
-    @broxy.source()
-    def test():
-        return 2
+    print(next(broxy._fetcher))
+    print(next(broxy._fetcher))
+    print(next(broxy._fetcher))
 
-    print(broxy.status())
-
+if __name__ == "__main__":
+    main()
