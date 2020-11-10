8212f86f871601ebc1acef9c16e6250ebc45248f
Liszt21 <1832666492@qq.com>
2020-11-10 13:55:16 +0800

    add thread to Broxy

--- src/broxy/__init__.py
+++ src/broxy/__init__.py
@@ -1,4 +1,27 @@
+import threading
+import time
+import inspect
+import ctypes
+
 __VERSION__ = "0.0.1"
+
+def _async_raise(tid, exctype):
+    """raises the exception, performs cleanup if needed"""
+    tid = ctypes.c_long(tid)
+    if not inspect.isclass(exctype):
+        exctype = type(exctype)
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, ctypes.py_object(exctype))
+    if res == 0:
+        raise ValueError("invalid thread id")
+    elif res != 1:
+        # """if it returns a number greater than one, you're in trouble,
+        # and you should call it again with exc=NULL to revert the effect"""
+        ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, None)
+        raise SystemError("PyThreadState_SetAsyncExc failed")
+ 
+ 
+def stop_thread(thread):
+    _async_raise(thread.ident, SystemExit)
 
 class Broxy():
     def __init__(self):
@@ -6,6 +29,8 @@ class Broxy():
         self._sources = {}
         self._statistic = {}
         self._fetcher = None
+        self._sever = None
+        self._thread = None
 
     def source(self, override=False):
         def decorator(f):
@@ -23,13 +48,35 @@ class Broxy():
                 yield proxy
             
     def get(self,num=1):
-        pass
+        l = len(self._pool)
+        return self._pool[:num if num < l else -1]
 
     def status(self):
         return "Pool   => total: {0}, usable: {1}\nSources=> total: {2}".format(len(self._pool), 0, len(self._sources))
 
     def verify(self, p):
         return False
+
+    def start(self):
+        self._thread = threading.Thread(target=self.run)
+        self._thread.start()
+
+    def stop(self):
+        if not self._thread:
+            return
+        stop_thread(self._thread)
+        self._thread = None
+        print("Thread stopped")
+
+    def __del__(self):
+        self.stop()
+
+    def run(self):
+        if len(self._sources) == 0:
+            return
+        while(len(self._pool) < 10):
+            self._pool.append(next(self._fetcher))
+            time.sleep(1);
 
 def main():
     print("Broxy!")
@@ -44,9 +91,10 @@ def main():
                 "protocol": "http"
             }
 
-    print(next(broxy._fetcher))
-    print(next(broxy._fetcher))
-    print(next(broxy._fetcher))
+    broxy.start()
+    time.sleep(2)
+    print(broxy.get(3))
+    broxy.stop()
 
 if __name__ == "__main__":
     main()
